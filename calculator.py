5a1669cd4fb61b8bd2220be2009e88f583e4cfe5
oligerani <131018499+oligerani@users.noreply.github.com>
2025-02-01 22:21:42 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,29 @@
+from abc import ABC,abstractmethod
+class ICalculator(ABC):
+    @abstractmethod
+    def add(self):
+        pass
+    def substract(self):
+        pass
+    def multiply(self):
+        pass
+    def divide(self):
+        pass
+class BasicCalculator(ICalculator):
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def add(self):
+        return self.a+self.b
+    def substract(self):
+        return self.a-self.b
+    def multiply(self):
+        return self.a*self.b 
+    def divide(self):
+        return self.a/self.b
+ob=BasicCalculator(4,2)
+print(ob.add())
+print(ob.substract())
+print(ob.divide())
+print(ob.multiply())   
+        
---
+++ database.py
@@ -0,0 +1,43 @@
+#Define an abstract class `IDatabaseOperations` with methods `insert()`, `update()`, and `delete()`. Implement this in `SQLDatabase` and `NoSQLDatabase`.
+from abc import ABC,abstractmethod
+class IDatabaseOperations(ABC):
+    
+    @abstractmethod
+    def insert(self):
+        pass
+    def update(self):
+        pass
+    
+    def delete(self):
+        pass
+class SQLDatabase(IDatabaseOperations):
+    def __init__(self,data):
+        self.data=data
+        
+    def insert(self):
+        
+        return f'the data is added {self.data}'
+    def update(self):
+        
+        return f'the data is updated {self.data}'
+    def delete(self):
+        
+        return f'the data is removed {self.data}' 
+class NoSQLDatabase(IDatabaseOperations):
+    def __init__(self,data):
+        self.data=data
+        
+    def insert(self):
+        
+        return f'the data is added {self.data}'
+    def update(self):
+        
+        return f'the data is updated {self.data}'
+    def delete(self):
+        
+        return f'the data is removed {self.data}'
+ob=NoSQLDatabase("user data")
+print(ob.insert())
+print(ob.delete())
+print(ob.update())   
+       
